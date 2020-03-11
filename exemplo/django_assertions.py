0f442c348df7baf30552098f5d57514a088e501a
renzon <renzon@gmail.com>
2020-03-11 09:19:29 -0300

    Implementada passagem de dados de cliente via query string
    close #48

--- exemplo/django_assertions.py
+++ exemplo/django_assertions.py
@@ -3,3 +3,4 @@ from django.test import TestCase
 _test_case = TestCase()
 
 assert_contains = _test_case.assertContains
+assert_not_contains = _test_case.assertNotContains
--- exemplo/pagamentos/views.py
+++ exemplo/pagamentos/views.py
@@ -9,7 +9,11 @@ authentication_key(settings.CHAVE_PAGARME_API_PRIVADA)
 
 
 def produto(request, slug: str):
+    open_modal = request.GET.get('open_modal', '').lower() == 'true'
+    customer_data = {k: request.GET.get(k, '') for k in ['name', 'email', 'phone']}
     ctx = {
-        'payment_item': facade.get_payment_item(slug)
+        'payment_item': facade.get_payment_item(slug),
+        'open_modal': open_modal,
+        'customer': customer_data,
     }
     return render(request, 'pagamentos/produto.html', ctx)
