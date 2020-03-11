ce2c64449ef7041fb1e5fe6843ddc968ab9fef97
renzon <renzon@gmail.com>
2020-03-11 10:01:43 -0300

    Extracted customer data from logged user
    close #51

--- exemplo/pagamentos/views.py
+++ exemplo/pagamentos/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+from collections import ChainMap
+
 from django.conf import settings
 from django.shortcuts import render
 from pagarme import authentication_key
@@ -10,10 +12,17 @@ authentication_key(settings.CHAVE_PAGARME_API_PRIVADA)
 
 def produto(request, slug: str):
     open_modal = request.GET.get('open_modal', '').lower() == 'true'
-    customer_data = {k: request.GET.get(k, '') for k in ['name', 'email', 'phone']}
+    customer_qs_data = {k: request.GET.get(k, '') for k in ['name', 'email', 'phone']}
+    customer_qs_data = {k: v for k, v in customer_qs_data.items() if v}
+    user = request.user
+    if user.is_authenticated:
+        user_data = {'external_id': user.id, 'name': user.first_name, 'email': user.email}
+        customer = ChainMap(customer_qs_data, user_data)
+    else:
+        customer = customer_qs_data
     ctx = {
         'payment_item': facade.get_payment_item(slug),
         'open_modal': open_modal,
-        'customer': customer_data,
+        'customer': customer,
     }
     return render(request, 'pagamentos/produto.html', ctx)
