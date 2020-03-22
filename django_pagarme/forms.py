4a7aedd9d86a21a22c10770f24bf435a84f267ac
renzon <renzon@gmail.com>
2020-03-22 08:35:34 -0300

    Changed Phones Label and Placeholder
    Released 0.6
    
    close #97

--- django_pagarme/forms.py
+++ django_pagarme/forms.py
@@ -5,4 +5,4 @@ from phonenumber_field.formfields import PhoneNumberField
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=64, label='Nome Completo')
     email = forms.EmailField()
-    phone = PhoneNumberField(label='Celular', widget=forms.TextInput(attrs={'placeholder': '(11) 98765-1854'}))
+    phone = PhoneNumberField(label='Celular. Ex: (12) 98765-4321 ', widget=forms.TextInput(attrs={'placeholder': '(XX) XXXXX-XXXX'}))
