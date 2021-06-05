d3b246d82c4722cc0f743ed5613d8be922a764ba
renzon <renzon@gmail.com>
2021-06-05 16:11:14 -0300

    Melhorado admin do Django para evitar carregamento de todos usuários
    close #162
    
    Alteradas permissões de adição e edição de entidades geradas automaticamente via integração com Pagarme
    
    Feita migração de chaves primárias para BigInteger

--- django_pagarme/apps.py
+++ django_pagarme/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DjangoPagarmeConfig(AppConfig):
     name = 'django_pagarme'
     verbose_name = "Dados do Pagarme"
+    default_auto_field = 'django.db.models.BigAutoField'
